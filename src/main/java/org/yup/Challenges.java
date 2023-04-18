28b7b970c9bb4364bcc93a7ce00bcc87efa8c860
Melissa Guzman <melissaaa.24.guz@gmail.com>
2023-04-18 09:37:53 -0400

    added several challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,4 +1,56 @@
 package org.yup;
+//both are in same package
 
 public class Challenges {
+
+    public String greetingWithUserInput{
+        if(age >= 21){
+            return "Hello, " + name + ", welcome to the party!";
+        } else{
+            //when condition is false
+            return "Hello, " + name + ", I am sorry but we cannot let you into the party";
+        }
+    }
+    public String greetingWithUserInput{
+
+    }
+        if( age >= 21){
+        return "Hello, " + name + ", welcome to the party!";
+        } else{
+            //when condition is false
+        return "Hello, " + name + ", I am sorry but we cannot let you into the party";
+        }
+}
+
+    public int addTwoNumbers(){
+
+        Scanner theScanner = new Scanner(System.in);
+
+        //ask the user for number 1
+        System.out.print("Give me another number, please. ");
+        int number1 = theScanner.nextInt();
+
+        //ask the user for number 2
+        System.out.print("Give me another number, please. ");
+        int number2 = theScanner.nextInt();
+
+        //return the sum of number1 and number 2 both
+        return number1 + number2;
+    }
+
+    //create a method like the one shone above, subtractNumbers
+    public int subtractTwoNumbers(){
+
+    Scanner theScanner = newScanner(System.in);
+
+    //ask user for number1
+    System.out.print("Give me a number please. ");
+    int nuber1 = theScanner.nextInt();
+
+    //ask for user number2
+        System.out.print("Give me another number please.");
+        int number2 = theScanner.nextInt();
+
+        return number1 - number2;
+
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,11 +1,46 @@
 package org.yup;
 
+import java.util.Scanner;
 public class JavaChallengesApp {
 
-    public static void main(String[] args){
 
-        System.out.println("Hello from the Java Challenges App");
+    //this runs greeting
+    //stores in greeting variable created
+    public static void main(String[] args) {
+        //this code is running greetings :)
+        //
+        // create new instance to make challenges in the app
+        Challenges challenges = new Challenges();
+        String greeting = challenges.greeting("Alex", 21);
+
+        System.out.println(greeting);
 
     }
 
+
+    // let ppl in if 21 and up
+// POLITELY say no if 21 and younger
+    public String greetingWithUserInput() {
+        Scanner theScanner = new Scanner(System.in);
+
+        //ask user name
+        System.out.print("Enter your name: ");
+        String name = theScanner.nextLine();
+
+        //ask user age
+        System.out.print("Enter your age: ");
+        int age = theScanner.nextInt();
+
+
+        // let ppl in if 21 and up
+        // POLITELY say no if 21 and younger
+        if (age >= 21) {
+            return "Hello, " + name + ", welcome to the party!";
+
+        } else {
+
+            //when condition is false
+            return "Hello, " + name + ", I am sorry but we cannot let you into the party";
+        }
+    }
 }
